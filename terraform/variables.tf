5c369a632a3ca52f52524ed49a9625850f27edfc
VladyslavTEX <vladyslavtex@gmail.com>
2025-07-07 09:17:23 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,9 @@
 variable "aws_region" {
   description = "AWS region"
-  type        = string
   default     = "us-east-1"
 }
 
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
-
 variable "ssh_public_key" {
-  description = "SSH public key for EC2 instances"
+  description = "SSH public key"
   type        = string
-}
-
-variable "environment_suffix" {
-  description = "Suffix for resource names to make them unique"
-  type        = string
-  default     = "default"
 }
