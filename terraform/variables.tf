8599a8bbe6eaf34886425553fa06f87d3b7e137d
VladyslavTEX <vladyslavtex@gmail.com>
2025-07-07 08:57:41 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,8 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
 }
+
+variable "ssh_public_key" {
+  description = "SSH public key for EC2 instances"
+  type        = string
+}
