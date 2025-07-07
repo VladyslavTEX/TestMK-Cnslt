9b1c0a541da282597e1cfee1b9b1b9a36e0cf7aa
VladyslavTEX <vladyslavtex@gmail.com>
2025-07-07 09:17:10 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  tags = {
-    Name = "WebServer"
-  }
+resource "random_id" "suffix" {
+  byte_length = 4
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key-${var.environment_suffix}"
+  key_name   = "deployer-key-${random_id.suffix.hex}"
   public_key = var.ssh_public_key
 }
 
 resource "aws_security_group" "web_sg" {
-  name        = "web_sg_${var.environment_suffix}"
-  description = "Allow HTTP/SSH for web server ${var.environment_suffix}"
+  name        = "web_sg_${random_id.suffix.hex}"
+  description = "Allow HTTP/SSH traffic"
 
   ingress {
     from_port   = 80
@@ -42,21 +48,27 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name = "web-sg-${var.environment_suffix}"
+    Name = "web-server-${random_id.suffix.hex}"
   }
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-
-  owners = ["099720109477"] # Canonical
 }
 
 output "public_dns" {
