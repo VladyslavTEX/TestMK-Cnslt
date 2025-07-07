8fe7e50c50b131be5fc7e1ffb39af704315bf158
VladyslavTEX <vladyslavtex@gmail.com>
2025-07-07 09:02:11 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,9 @@ variable "ssh_public_key" {
   description = "SSH public key for EC2 instances"
   type        = string
 }
+
+variable "environment_suffix" {
+  description = "Suffix for resource names to make them unique"
+  type        = string
+  default     = "default"
+}
