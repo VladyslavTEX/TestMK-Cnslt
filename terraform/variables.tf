bf922b99bb81bc234ee91f8c4e716774bcc28141
VladyslavTEX <vladyslavtex@gmail.com>
2025-07-07 08:49:19 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,1 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-east-1"
-}
 
-variable "instance_type" {
-  description = "Type of EC2 instance"
-  defaul      = "t2.micro"
-}
